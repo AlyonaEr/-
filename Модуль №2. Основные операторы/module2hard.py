36586aa09b84139c915ec0bf38585f4afd33f032
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-07-24 20:21:37 +0500

    initial

--- Модуль №2. Основные операторы/module2hard.py
+++ Модуль №2. Основные операторы/module2hard.py
@@ -1,1 +1,9 @@
-result = []
+import random
+
+n = random.randint(3,20)
+result = ''
+for i in range(1,n):
+    for j in range (i+1, n):
+        if n % (i+j) == 0:
+            result+=f"{i}{j}"
+print(n, result)
