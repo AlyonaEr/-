5237f386585e54d00b8c89a0abc4d478f2beb950
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-07-27 16:52:19 +0500

    initial

---
+++ Модуль №1. Практика GIT. Базовые структуры данных/Базовые структуры данных (старый вариант).py
@@ -0,0 +1,28 @@
+a = "Запишите в переменную a произвольную строку. Затем вычислите длину строки и выведите результат на экран"
+print(len(a))
+
+first = 25
+second = 235
+summa = first + second
+diff = first - second
+print(f'{summa} {diff}')
+
+first = 256
+second = 15
+third = 58
+s = [first, second, third]
+mean = sum(s)/len(s)
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+print(f'{second_string}, {first_string}')
+
+a = 12
+b = 16
+c = 2
+f = (a * b) + (a * c)
+print(f)
+print((f**3)/2)
+
