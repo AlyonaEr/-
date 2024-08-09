8ef2e8675cd761903d72a349575ac0158f3eb5e6
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-08-09 22:16:18 +0500

    1

--- Тренировка/тр3.py
+++ Тренировка/тр3.py
@@ -2,7 +2,7 @@ nums = [9, 6, 3, 5, 8, 4, 2, 7, 1]
 
 
 def bubble_sort(ls):
-    for i in range(0, len(ls) - 1):
+    for i in range(len(ls) - 1):
         for j in range(len(ls) - 1 - i):
             if ls[j] > ls[j + 1]:
                 ls[j], ls[j + 1] = ls[j + 1], ls[j]
