297b151d8896dd37033625dc9b0d4663393be918
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-08-12 21:48:47 +0500

    initial

---
+++ Модуль №5. Классы и объекты/help.py
@@ -0,0 +1,11 @@
+def go_to(new_floor):
+    floors = 1, new_floor
+    if new_floor > number_of_floors or new_floor < 1:
+        print("Такого этажа не существует")
+    else:
+        print(*list(range(1, int(new_floor) + 1)), sep='\n')
+
+
+number_of_floors = 7
+go_to(5)
+go_to(10)
