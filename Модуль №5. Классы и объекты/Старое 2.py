95a0d38c9759b929688eed658983afe1ebaa5d13
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-08-15 21:32:28 +0500

    1

---
+++ Модуль №5. Классы и объекты/Старое 2.py
@@ -0,0 +1,20 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+
+    def __str__(self):
+        return f'Кол-во этажей: {self.numberOfFloors}, название: {self.buildingType}'
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors
+        return self.buildingType == other.buildingType
+
+
+h1 = Building(10, 'ЖК Эльбрус')
+h2 = Building(20, 'ЖК Акация')
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
