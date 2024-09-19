f7c10a74224b6b8cca578ceb7e45341210873424
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-09-19 19:42:37 +0500

    initial

--- Модуль №8 Исключения/module_8_1.py
+++ Модуль №8 Исключения/module_8_1.py
@@ -1,11 +1,10 @@
 def add_everything_up(a, b):
     try:
-        print(a + b)
+        print (a + b)
     except TypeError as exc:
         print(f'вот что пошло не так - {exc}, но мы ещё на плаву')
         print(f'{a}{b}')
 
-
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
