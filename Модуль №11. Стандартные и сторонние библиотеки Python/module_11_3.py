c622966b84f1c28e832f1cc46d9e463a3e0f8d42
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-11-21 21:21:39 +0500

    1

--- Модуль №11. Стандартные и сторонние библиотеки Python/module_11_3.py
+++ Модуль №11. Стандартные и сторонние библиотеки Python/module_11_3.py
@@ -8,11 +8,19 @@ class Pets:
 
 def introspection_info(obj):
     return (f'type: {type(obj)}\n'
-            f'attributes: {getattr(obj, 'name')}\n'
+            f'attributes: {getattr(obj, 'name', 'атрибут отсутствует')}\n'
             f'methods: {dir(obj)}\n'
             f'module: {inspect.getmodule(obj)}')
 
 
 pets_1 = Pets("Alisa")
-number_info = introspection_info(pets_1)
-print(number_info)
+list_1 = ['Sea', 12, ('red', 'blue', 1)]
+string_1 = 'i am a string'
+number_info_1 = introspection_info(pets_1)
+print(number_info_1)
+number_info_2 = introspection_info(24)
+print(number_info_2)
+number_info_3 = introspection_info(list_1)
+print(number_info_3)
+number_info_4 = introspection_info(string_1)
+print(number_info_4)
